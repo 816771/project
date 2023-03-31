1f9f5f4be49442daea68ca28f2dd0c1fec6ad198
816771 <129550419+816771@users.noreply.github.com>
2023-03-31 14:37:30 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,66 @@
+function rob(){
+  document.getElementById("plop").innerHTML =  "Robbie you are trash";
+  setTimeout(() => { document.getElementById("plop").innerHTML =  "";  }, 1000);
+}
+
+
+
+
+
+
+ function slideOne(){
+ document.getElementById("plop").innerHTML =  "hola";
+   
+    }
+
+
+ function slideTwo(){
+   document.getElementById("plop").innerHTML = "New text!";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  var checkbox = document.querySelector('input[type="checkbox"]');
+
+  checkbox.addEventListener('change', function () {
+    if (checkbox.checked) {
+      // do this
+      console.log('Checked');
+       var element = document.getElementById("topbar");
+        element.style.opacity = "0.0";
+    } else {
+      // do that
+      console.log('Not checked');
+       var element = document.getElementById("topbar");
+        element.style.opacity = "1.0";
+    }
+  });
+});
+
+
+
+
+const buttons = document.getElementsByTagName("button");
+const result = document.getElementById("result");
+
+const buttonPressed = e => { 
+  result.innerHTML = `ID of <em>${e.target.innerHTML}</em> is <strong>${e.target.id}</strong>`;
+}
+
+for (let button of buttons) {
+  button.addEventListener("click", buttonPressed);
+}
