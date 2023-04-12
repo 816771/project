be96a58b24f11c8fd090d73d437f7f6772681baa
816771 <129550419+816771@users.noreply.github.com>
2023-04-12 20:49:31 +0000

    modified:   index.html 	modified:   initial.css 	modified:   initial.js

--- initial.js
+++ initial.js
@@ -1,6 +1,7 @@
 function myFunction() {
-    let person = prompt("Please enter your name", "");
+    let person = prompt("Yeah i know, alert boxes suck.", "");
     if (person === "1234") {
-alert("correct input, acces alowowoed");
+        document.getElementById("link").href="main.html";
+        document.getElementById("link").innerHTML ="Actual Knowledge";
     }
 }
