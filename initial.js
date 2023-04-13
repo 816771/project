cc578c407bbb9be7f9bbeb942fa60c1fa67be669
816771 <129550419+816771@users.noreply.github.com>
2023-04-13 14:39:20 +0000

    modified:   index.html 	modified:   initial.js

--- initial.js
+++ initial.js
@@ -1,6 +1,28 @@
+var a = Math.floor(Math.random() * (2 - 1 + 1) ) + 1;
+
+if(a == 1)
+{
+   var password = "grayson_sucks";
+}
+else if(a == 2)
+{
+    var password = "robbie_sucks";
+}
+
+
+
+
+function nopode() {
+
+    document.getElementById("flip").innerHTML = a + " " + password;
+}
+
+
+
+
 function myFunction() {
     let person = prompt("Yeah i know, alert boxes suck.", "");
-    if (person === "1234") {
+    if (person === password) {
         document.getElementById("link").href="main.html";
         document.getElementById("link").innerHTML ="Actual Knowledge";
     }
